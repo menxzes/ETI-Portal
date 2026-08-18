65fb311e524b67376245ffec520cb59f902cc1f4
menxzes <glhrm276@gmail.com>
2026-08-17 21:14:09 -0300

    fix: corrige o import em core/settings.py

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 MEUS IMPORTS
 """
 import os
-import dj_databse_url
+import dj_database_url
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
