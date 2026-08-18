f09f152c2a30ba45d9155c92f0b7f97f744c8715
menxzes <glhrm276@gmail.com>
2026-08-17 21:11:03 -0300

    feat: cria o model de usuario

--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Usuario(AbstractUser):
+    is_aluno = models.BooleanField(default=False)
+    is_professor = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
