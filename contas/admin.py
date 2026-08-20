b03c568b3506e40dd39f43a4443a69a5502cd2f2
menxzes <glhrm276@gmail.com>
2026-08-19 23:15:05 -0300

    feat: configura admin do app contas

--- contas/admin.py
+++ contas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Perfis', {'fields': ('is_aluno', 'is_professor')}),
+    )
